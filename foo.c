12762ce11ec672f75f4f0eb937f289799de99e89
Gbkng <guillaume.brooking@gmail.com>
2025-07-27 17:45:41 +0200

    fix: improve avoiding unused warnings

--- foo.c
+++ foo.c
@@ -2,10 +2,10 @@ static const int a[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 static int b[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 static int foo(void) {return 4;}
 int bar(void) {return 4;}
-static void dumb(void){
+
+void avoid_unused_warnings(void){
   (void)a;
   (void)b;
   (void)foo;
   (void)bar;
-  (void)dumb;
 }
